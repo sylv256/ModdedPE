8a860a8ab830d7c266ee78f6c229ded4e1765479
ModelPart <2286108831@qq.com>
2017-06-15 20:26:34 +0800

    uses android studio~

---
+++ ModdedPE-Project/app/src/main/java/com/mcal/ModdedPE/app/MainActivity.java
@@ -0,0 +1,254 @@
+package com.mcal.ModdedPE.app;
+import android.content.*;
+import android.os.*;
+import android.support.v4.app.*;
+import android.support.v4.view.*;
+import android.support.v7.app.*;
+import android.support.v7.widget.*;
+import android.view.*;
+import com.mcal.ModdedPE.*;
+import com.mcal.ModdedPE.utils.*;
+import java.util.*;
+
+public class MainActivity extends BaseActivity
+{
+	private ViewPager mMainViewPager;
+	private MainManageNModFragment mManageNModFragment;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.moddedpe_main_pager);
+
+		ArrayList<Fragment> fragment_list=new ArrayList<Fragment>();
+		ArrayList<CharSequence> titles_list=new ArrayList<CharSequence>();
+
+		MainStartFragment startFragment = new MainStartFragment();
+		fragment_list.add(startFragment);
+		titles_list.add(getString(R.string.main_title));
+
+		mManageNModFragment = new MainManageNModFragment();
+		fragment_list.add(mManageNModFragment);
+		titles_list.add(getString(R.string.manage_nmod_title));
+
+		MainSettingsFragment settingsFragment = new MainSettingsFragment();
+		fragment_list.add(settingsFragment);
+		titles_list.add(getString(R.string.settings_title));
+
+		MainFragmentPagerAdapter pagerAdapter = new MainFragmentPagerAdapter(fragment_list, titles_list);
+
+		mMainViewPager = (ViewPager) findViewById(R.id.moddedpe_main_view_pager);
+		mMainViewPager.setAdapter(pagerAdapter);
+		mMainViewPager.setOnPageChangeListener(new ViewPager.OnPageChangeListener()
+			{
+				@Override
+				public void onPageScrolled(int p1, float p2, int p3)
+				{
+					setTitle(mMainViewPager.getAdapter().getPageTitle(p1));
+				}
+
+				@Override
+				public void onPageSelected(int p1)
+				{
+
+				}
+
+				@Override
+				public void onPageScrollStateChanged(int p1)
+				{
+
+				}
+			});
+	}
+
+	@Override
+	protected void setDefaultActionBar()
+	{
+		super.setDefaultActionBar();
+
+		final View burgerButton=getLayoutInflater().inflate(R.layout.moddedpe_ui_button_menu, null);
+		burgerButton.findViewById(R.id.moddedpe_ui_button_item_image_button).setOnClickListener(new View.OnClickListener()
+			{
+
+				@Override
+				public void onClick(View p1)
+				{
+					PopupMenu popup =new PopupMenu(MainActivity.this, burgerButton);
+                    popup.getMenuInflater().inflate(R.menu.moddedpe_main_menu, popup.getMenu());
+                    popup.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener()
+						{
+							public boolean onMenuItemClick(MenuItem item)
+							{
+								switchViewPager(item);
+								return true;
+							}
+						});
+					popup.show();
+				}
+			});
+		setActionBarViewRight(burgerButton);
+
+		View imageButtonCreeper=getLayoutInflater().inflate(R.layout.moddedpe_ui_button_creeper, null);
+		imageButtonCreeper.findViewById(R.id.moddedpe_ui_button_item_image_button).setOnClickListener(new View.OnClickListener()
+			{
+
+				@Override
+				public void onClick(View p1)
+				{
+					startActivity(new Intent(MainActivity.this, AboutActivity.class));
+				}
+
+			});
+		setActionBarViewLeft(imageButtonCreeper);
+	}
+
+	private void switchViewPager(MenuItem item)
+	{
+		switch (item.getItemId())
+		{
+			case R.id.item_main_page:
+				mMainViewPager.setCurrentItem(0, true);
+				break;
+			case R.id.item_manage_nmods:
+				mMainViewPager.setCurrentItem(1, true);
+				break;
+			case R.id.item_launcher_settings:
+				mMainViewPager.setCurrentItem(2, true);
+				break;
+		}
+	}
+
+	public void onPlayClicked(View v)
+	{
+		if (!getPESdk().getMinecraftInfo().isMinecraftInstalled())
+		{
+			android.support.v7.app.AlertDialog.Builder mdialog = new AlertDialog.Builder(this);
+			mdialog.setTitle(getString(R.string.no_mcpe_found_title));
+			mdialog.setMessage(getString(R.string.no_mcpe_found));
+			mdialog.setPositiveButton(getString(android.R.string.cancel), new DialogInterface.OnClickListener()
+				{
+
+					@Override
+					public void onClick(DialogInterface p1, int id)
+					{
+						p1.dismiss();
+					}
+				});
+			mdialog.show();
+		}
+		else if (!getPESdk().getMinecraftInfo().isSupportedMinecraftVersion(getResources().getStringArray(R.array.target_mcpe_versions)))
+		{
+			android.support.v7.app.AlertDialog.Builder mdialog = new AlertDialog.Builder(this);
+			mdialog.setTitle(getString(R.string.no_available_mcpe_version_found_title));
+			mdialog.setMessage(getString(R.string.no_available_mcpe_version_found, new Object[]{getPESdk().getMinecraftInfo().getMinecraftVersionName(),getString(R.string.target_mcpe_version_info)}));
+			mdialog.setNegativeButton(getString(android.R.string.cancel), new DialogInterface.OnClickListener()
+				{
+
+					@Override
+					public void onClick(DialogInterface p1, int id)
+					{
+						p1.dismiss();
+					}
+				});
+			mdialog.setPositiveButton(getString(R.string.no_available_mcpe_version_continue), new DialogInterface.OnClickListener()
+				{
+
+					@Override
+					public void onClick(DialogInterface p1, int id)
+					{
+						startMinecraft();
+					}
+
+
+				});
+			mdialog.show();
+			return;
+		}
+		else
+			startMinecraft();
+	}
+
+	private void startMinecraft()
+	{
+		if (new UtilsSettings(MainActivity.this).isSafeMode())
+		{
+			new AlertDialog.Builder(this).setTitle(R.string.safe_mode_on_title).setMessage(R.string.safe_mode_on_message).
+				setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener()
+				{
+
+					@Override
+					public void onClick(DialogInterface p1, int p2)
+					{
+						Intent intent = null;
+						intent = new Intent(MainActivity.this, PreloadActivity.class);
+						startActivity(intent);
+						finish();
+						p1.dismiss();
+					}
+
+
+				}).setNegativeButton(android.R.string.cancel, new DialogInterface.OnClickListener()
+				{
+
+					@Override
+					public void onClick(DialogInterface p1, int p2)
+					{
+						p1.dismiss();
+					}
+
+
+				}).show();
+		}
+		else
+		{
+			Intent intent = null;
+			intent = new Intent(MainActivity.this, PreloadActivity.class);
+			startActivity(intent);
+			finish();
+		}
+	}
+
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data)
+	{
+		super.onActivityResult(requestCode, resultCode, data);
+		mManageNModFragment.onActivityResult(requestCode, resultCode, data);
+	}
+
+	private class MainFragmentPagerAdapter extends FragmentPagerAdapter
+	{
+		private List<Fragment> mFragments;
+		private List<CharSequence> mTitles;
+		public MainFragmentPagerAdapter(List<Fragment>fragments, List<CharSequence>titles)
+		{
+			super(getSupportFragmentManager());
+			this.mFragments = fragments;
+			this.mTitles = titles;
+		}
+
+		@Override
+		public int getCount()
+		{
+			return mFragments.size();
+		}
+
+		@Override
+		public Fragment getItem(int p1)
+		{
+			return mFragments.get(p1);
+		}
+
+		@Override
+		public long getItemId(int position)
+		{
+			return position;
+		}
+
+		@Override
+		public CharSequence getPageTitle(int position)
+		{
+			return mTitles.get(position);
+		}
+	}
+}
---
+++ ModdedPE-Project/app/src/main/java/com/mcal/ModdedPE/app/NModLoadFailActivity.java
@@ -0,0 +1,166 @@
+package com.mcal.ModdedPE.app;
+
+import android.content.*;
+import android.os.*;
+import android.support.v7.widget.*;
+import com.mcal.ModdedPE.*;
+import com.mcal.pesdk.nmod.*;
+import java.util.*;
+import android.widget.*;
+import android.view.*;
+import android.app.*;
+import android.graphics.*;
+import java.io.*;
+
+public class NModLoadFailActivity extends BaseActivity
+{
+	private static final String KEY_TYPE_STRING = "type_string";
+	private static final String KEY_MESSAGE = "message";
+	private static final String KEY_TYPE = "type";
+	private static final String KEY_PACKAGE_NAME = "package_name";
+	private static final String KEY_ICON_PATH = "icon_path";
+	private static final String KEY_MC_DATA = "mc_data";
+
+	private ArrayList<Integer> mTypes = new ArrayList<Integer>();
+	private ArrayList<String> mPackageNames = new ArrayList<String>();
+	private ArrayList<String> mMessages = new ArrayList<String>();
+	private ArrayList<String> mTypeStrings = new ArrayList<String>();
+	private ArrayList<String> mIconPaths = new ArrayList<String>();
+	private Bundle mMCData;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.moddedpe_nmod_load_failed);
+
+		mTypes = getIntent().getExtras().getIntegerArrayList(KEY_TYPE);
+		mMessages = getIntent().getExtras().getStringArrayList(KEY_MESSAGE);
+		mIconPaths = getIntent().getExtras().getStringArrayList(KEY_ICON_PATH);
+		mTypeStrings = getIntent().getExtras().getStringArrayList(KEY_TYPE_STRING);
+		mPackageNames = getIntent().getExtras().getStringArrayList(KEY_PACKAGE_NAME);
+		mMCData = getIntent().getExtras().getBundle(KEY_MC_DATA);
+
+		ListView errorListView = (ListView)findViewById(R.id.nmod_load_failed_list_view);
+		errorListView.setAdapter(new ViewAdapter());
+	}
+
+	private class BuggedNModInfo
+	{
+		public String mTypeString;
+		public String mMessage;
+		public int mType;
+		public String mPackageName;
+		public String mIconPath;
+	}
+
+	private class ViewAdapter extends BaseAdapter
+	{
+		@Override
+		public int getCount()
+		{
+			return mPackageNames.size();
+		}
+
+		@Override
+		public Object getItem(int p1)
+		{
+			return p1;
+		}
+
+		@Override
+		public long getItemId(int p1)
+		{
+			return p1;
+		}
+
+		@Override
+		public View getView(int p1, View p2, ViewGroup p3)
+		{
+			CardView view = (CardView)getLayoutInflater().inflate(R.layout.moddedpe_nmod_load_failed_item_card, null);
+			AppCompatTextView packageNameTextView = (AppCompatTextView)view.findViewById(R.id.moddedpe_nmod_load_failed_item_card_package_name);
+			packageNameTextView.setText(mPackageNames.get(p1));
+			AppCompatTextView errorMessageTextView = (AppCompatTextView)view.findViewById(R.id.moddedpe_nmod_load_failed_item_card_message);
+			errorMessageTextView.setText(getString(R.string.load_fail_msg, new Object[]{mTypeStrings.get(p1),mMessages.get(p1)}));
+			AppCompatImageView imageViewIcon = (AppCompatImageView)view.findViewById(R.id.moddedpe_nmod_load_failed_item_card_icon);
+			try
+			{
+				if (mIconPaths.get(p1) != null)
+					imageViewIcon.setImageBitmap(BitmapFactory.decodeStream(new FileInputStream(mIconPaths.get(p1))));
+				else
+					imageViewIcon.setImageBitmap(BitmapFactory.decodeResource(getResources(), R.drawable.mcd_null_pack));
+			}
+			catch (FileNotFoundException e)
+			{
+				imageViewIcon.setImageBitmap(BitmapFactory.decodeResource(getResources(), R.drawable.mcd_null_pack));
+			}
+			final int index = p1;
+			view.setOnClickListener(new View.OnClickListener()
+				{
+
+					@Override
+					public void onClick(View p1)
+					{
+						new AlertDialog.Builder(NModLoadFailActivity.this).setTitle(R.string.load_fail_title).setMessage(getString(R.string.load_fail_msg, new Object[]{mTypeStrings.get(index),mMessages.get(index)})).setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener()
+							{
+
+								@Override
+								public void onClick(DialogInterface p1, int p2)
+								{
+									p1.dismiss();
+								}
+
+
+							}).show();
+					}
+				});
+			return view;
+		}
+	}
+
+	public void onNextClicked(View view)
+	{
+		Intent intent = new Intent(this, MinecraftActivity.class);
+		intent.putExtras(mMCData);
+		startActivity(intent);
+		finish();
+	}
+
+	@Override
+	public void onBackPressed()
+	{
+
+	}
+
+	public static void startThisActivity(Context context, ArrayList<NMod> nmods, Bundle data)
+	{
+		Intent intent=new Intent(context, NModLoadFailActivity.class);
+		intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+		Bundle bundle = new Bundle();
+		ArrayList<Integer> mTypes = new ArrayList<Integer>();
+		ArrayList<String> mPackageNames = new ArrayList<String>();
+		ArrayList<String> mMessages = new ArrayList<String>();
+		ArrayList<String> mTypeStrings = new ArrayList<String>();
+		ArrayList<String> mIconPaths = new ArrayList<String>();
+		for (NMod nmod:nmods)
+		{
+			mTypes.add(nmod.getLoadException().getType());
+			mPackageNames.add(nmod.getPackageName());
+			mMessages.add(nmod.getLoadException().getCause().toString());
+			mTypeStrings.add(nmod.getLoadException().toTypeString());
+			File iconPath = nmod.copyIconToData();
+			if (iconPath != null)
+				mIconPaths.add(iconPath.getAbsolutePath());
+			else
+				mIconPaths.add(null);
+		}
+		bundle.putIntegerArrayList(KEY_TYPE, mTypes);
+		bundle.putStringArrayList(KEY_MESSAGE, mMessages);
+		bundle.putStringArrayList(KEY_ICON_PATH, mIconPaths);
+		bundle.putStringArrayList(KEY_TYPE_STRING, mTypeStrings);
+		bundle.putStringArrayList(KEY_PACKAGE_NAME, mPackageNames);
+		bundle.putBundle(KEY_MC_DATA, data);
+		intent.putExtras(bundle);
+		context.startActivity(intent);
+	}
+}
