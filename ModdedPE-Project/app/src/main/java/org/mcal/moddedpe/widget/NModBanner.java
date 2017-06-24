9a9102884ca0dd3b14d8839a69ad20e59ce3d5c0
ModelPart <2286108831@qq.com>
2017-06-24 15:44:00 +0800

    ui update

--- ModdedPE-Project/app/src/main/java/org/mcal/moddedpe/widget/NModBanner.java
+++ ModdedPE-Project/app/src/main/java/org/mcal/moddedpe/widget/NModBanner.java
@@ -2,6 +2,7 @@ package org.mcal.moddedpe.widget;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
+import android.graphics.Canvas;
 import android.os.Handler;
 import android.os.Message;
 import android.support.v4.view.PagerAdapter;
@@ -20,18 +21,15 @@ import org.mcal.moddedpe.app.NModDescriptionActivity;
 import org.mcal.pesdk.nmod.NMod;
 
 import java.util.ArrayList;
+import java.util.Random;
+import android.view.ViewGroup.*;
 
 @SuppressLint({ "InflateParams", "ClickableViewAccessibility" })
 public class NModBanner extends RelativeLayout
 {
-	private ViewPager mPager;
-	private BannerHandler mHandler;
-	private BannerThread mBannerThread = new BannerThread();
+	private RelativeLayout mBannerView;
+	private Random mRandom = new Random();
 	private ArrayList<NMod> mNModArrayList = new ArrayList<NMod>();
-
-	private static final int SLEEP_TIME = 3500;
-	private static final int MSG_UPDATE_NMODS = 1;
-	private static final int MSG_DO_SCROLL = 2;
 
 	public NModBanner(Context context)
 	{
@@ -58,53 +56,35 @@ public class NModBanner extends RelativeLayout
 		{
 			mNModArrayList.clear();
 			mNModArrayList.addAll(newNModList);
-			mPager.setAdapter(new BannerItemAdapater());
+			if (newNModList.size() > 0)
+				mBannerView = createBannerItemFor(newNModList.get(mRandom.nextInt(newNModList.size())));
+			removeAllViews();
+			addView(mBannerView);
 		}
+		invalidate();
 	}
 
 	private void init()
 	{
-		mPager = new ViewPager(getContext());
+		mBannerView = createEmptyBannerItem();
+		addView(mBannerView);
 		updateNModList();
-		addView(mPager);
-	}
-
-	@SuppressLint("HandlerLeak")
-	private class BannerHandler extends Handler
-	{
-
-		@Override
-		public void handleMessage(Message msg)
-		{
-			if (msg.what == MSG_UPDATE_NMODS)
-			{
-				updateNModList();
-			}
-			else if (msg.what == MSG_DO_SCROLL)
-			{
-				scrollToNext();
-			}
-		}
-	}
-
-	public void scrollToNext()
-	{
-		int index = mPager.getCurrentItem() + 1;
-		mPager.setCurrentItem(index % (Math.max(mNModArrayList.size(), 1)));
+		setWillNotDraw(false);
 	}
 
 	@Override
-	protected void onAttachedToWindow()
+	protected void onDraw(Canvas canvas)
 	{
-		super.onAttachedToWindow();
-		mBannerThread.start();
+		super.onDraw(canvas);
+		updateNModList();
 	}
 
 	@Override
-	protected void onDetachedFromWindow()
+	public void addView(View child)
 	{
-		mBannerThread.setStopped();
-		super.onDetachedFromWindow();
+		RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(LayoutParams.MATCH_PARENT,LayoutParams.MATCH_PARENT);
+		params.addRule(RelativeLayout.CENTER_IN_PARENT);
+		super.addView(child, params);
 	}
 
 	private RelativeLayout createEmptyBannerItem()
@@ -132,75 +112,5 @@ public class NModBanner extends RelativeLayout
 
 			});
 		return view;
-	}
-
-
-	private class BannerItemAdapater extends PagerAdapter
-	{
-		@Override
-		public int getCount()
-		{
-			return Math.max(mNModArrayList.size(), 1);
-		}
-
-		@Override
-		public boolean isViewFromObject(View arg0, Object arg1)
-		{
-			return arg0 == arg1;
-		}
-
-		@Override
-		public void destroyItem(ViewGroup container, int position, Object object)
-		{
-			container.removeView((View)object);
-		}
-
-		@Override
-		public Object instantiateItem(ViewGroup container, int position)
-		{
-			if (mNModArrayList.size() <= 0)
-			{
-				View view = createEmptyBannerItem();
-				container.addView(view);
-				return view;
-			}
-			View view = createBannerItemFor(mNModArrayList.get(position));
-			container.addView(view);
-			return view;
-		}
-	}
-
-	private class BannerThread extends Thread
-	{
-		private boolean stopped = false;
-
-		@Override
-		public void run()
-		{
-			while (!stopped)
-			{
-				try
-				{
-					Thread.sleep(SLEEP_TIME);
-				}
-				catch (InterruptedException e)
-				{}
-
-				try
-				{
-					if (!stopped)
-						mHandler.sendEmptyMessage(MSG_UPDATE_NMODS);
-					if (!stopped)
-						mHandler.sendEmptyMessage(MSG_DO_SCROLL);
-				}
-				catch (Throwable t)
-				{}
-			}
-		}
-
-		public void setStopped()
-		{
-			stopped = true;
-		}
 	}
 }
