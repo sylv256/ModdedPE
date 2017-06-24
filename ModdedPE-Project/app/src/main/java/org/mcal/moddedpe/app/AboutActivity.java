93849c16f488e69becbb03a9e5065a42988a902c
ModelPart <2286108831@qq.com>
2017-06-24 16:22:25 +0800

    onClick -> setOnClickListener

--- ModdedPE-Project/app/src/main/java/org/mcal/moddedpe/app/AboutActivity.java
+++ ModdedPE-Project/app/src/main/java/org/mcal/moddedpe/app/AboutActivity.java
@@ -14,46 +14,74 @@ public class AboutActivity extends BaseActivity
 	private static final String URI_GITHUB = "https://github.com/ModelPart/ModdedPE.git";
 	private static final String URI_PRIVACY_POLICY = "https://github.com/ModelPart/ModdedPE/blob/master/PrivacyPolicy/README.md";
 	private static final String URI_NMOD_API = "http://github.com/ModelPart/NModAPI.git";
-	
+
 	@Override
 	public void onCreate(Bundle savedInstanceState)
 	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.moddedpe_about);
-		
+
 		setActionBarButtonCloseRight();
-	}
-	
-	public void onViewGithubClicked(View view)
-	{
-		openUri(URI_GITHUB);
-	}
-	
-	public void onViewPrivacyPolicyClicked(View view)
-	{
-		openUri(URI_PRIVACY_POLICY);
-	}
-	
-	public void onViewNModAPIClicked(View view)
-	{
-		openUri(URI_NMOD_API);
-	}
-	
-	public void onViewTranslatorsClicked(View view)
-	{
-		new AlertDialog.Builder(this).setTitle(R.string.about_translators).setMessage(R.string.about_translators_message).setPositiveButton(android.R.string.ok,new DialogInterface.OnClickListener()
+
+		findViewById(R.id.about_view_github_button).setOnClickListener(new View.OnClickListener()
 			{
 
 				@Override
-				public void onClick(DialogInterface p1, int p2)
+				public void onClick(View p1)
 				{
-					p1.dismiss();
+					openUri(URI_GITHUB);
 				}
-				
-			
-		}).show();
+
+
+			});
+
+		findViewById(R.id.about_view_nmod_api_button).setOnClickListener(new View.OnClickListener()
+			{
+
+				@Override
+				public void onClick(View p1)
+				{
+					openUri(URI_NMOD_API);
+				}
+
+
+			});
+
+		findViewById(R.id.about_view_privacy_policy_button).setOnClickListener(new View.OnClickListener()
+			{
+
+				@Override
+				public void onClick(View p1)
+				{
+					openUri(URI_PRIVACY_POLICY);
+				}
+
+
+			});
+
+		findViewById(R.id.about_translators_button).setOnClickListener(new View.OnClickListener()
+			{
+
+				@Override
+				public void onClick(View p1)
+				{
+					new AlertDialog.Builder(AboutActivity.this).setTitle(R.string.about_translators).setMessage(R.string.about_translators_message).setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener()
+						{
+
+							@Override
+							public void onClick(DialogInterface p1, int p2)
+							{
+								p1.dismiss();
+							}
+
+
+						}).show();
+				}
+
+
+			});
 	}
-	
+
 	private void openUri(String uri)
 	{
 		Intent intent = new Intent();
